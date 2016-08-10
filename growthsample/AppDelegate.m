90fe57a43a7bf6083c8ea1346809c682b366de8e
y.uchiyama <yosuke.u0710@gmail.com>
2016-08-10 16:10:22 +0900

    growthsampleをbeatの2系に対応

--- growthsample/AppDelegate.m
+++ growthsample/AppDelegate.m
@@ -22,14 +22,15 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     [[Growthbeat sharedInstance] initializeWithApplicationId:GBApplicationId credentialId:GBCredentilaId];
-    [[GrowthPush sharedInstance] requestDeviceTokenWithEnvironment:kGrowthPushEnvironment];
+    [[GrowthPush sharedInstance] initializeWithApplicationId:GBApplicationId credentialId:GBCredentilaId environment:kGrowthPushEnvironment];
     [[GrowthPush sharedInstance] setDeviceTags];
+    [[GrowthPush sharedInstance] requestDeviceToken];
     
     NSMutableArray *intentHandlers = [NSMutableArray array];
     [intentHandlers addObject:[[GBUrlIntentHandler alloc] init]];
     [intentHandlers addObject:[[GBNoopIntentHandler alloc] init]];
     [intentHandlers addObject:[[MyCustomIntenHandler alloc] init]];
-    [[GrowthbeatCore sharedInstance] setIntentHandlers:intentHandlers];
+    [[Growthbeat sharedInstance] setIntentHandlers:intentHandlers];
     
     return YES;
 }
@@ -37,7 +38,7 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    [[Growthbeat sharedInstance] stop];
+
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -51,9 +52,7 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    [[Growthbeat sharedInstance] start];
-    [[GrowthAnalytics sharedInstance] purchase:100 setCategory:@"energy" setProduct:@"満タンドリンク"];
-//    [[GrowthPush sharedInstance] trackEvent:@"Launch"];
+    [[GrowthPush sharedInstance] trackEvent:@"Launch"];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
